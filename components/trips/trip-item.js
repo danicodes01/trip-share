8551b64df3f8fd97de10b6cbb01171d4a8afa3ec
Daniel <danoladesign@gmail.com>
2024-02-27 16:31:43 -0500

    TS016: Wrapped each trip item in a link component to make trip clickable / Set the href with the trips/<slug>, the slug from each show is accessible through props

--- components/trips/trip-item.js
+++ components/trips/trip-item.js
@@ -1,21 +1,31 @@
 import Image from 'next/image'
 import classes from './trip-item.module.css'
-export default function TripItem ({ title, image, summary, location, author }) {
+import Link from 'next/link'
+export default function TripItem ({
+  title,
+  image,
+  slug,
+  summary,
+  location,
+  author
+}) {
   return (
-    <article className={classes.trip}>
-      <header>
-        <div className={classes.image}>
-          <Image src={image} alt={title} fill />
+    <Link className={classes.link} href={`/trips/${slug}`}>
+      <article className={classes.trip}>
+        <header>
+          <div className={classes.image}>
+            <Image src={image} alt={title} fill />
+          </div>
+          <div className={classes.info}>
+            <p>{title}</p>
+            <p>{location}</p>
+            <p>by {author}</p>
+          </div>
+        </header>
+        <div className={classes.discription}>
+          <p className={classes.summary}>{summary}</p>
         </div>
-        <div className={classes.info}>
-          <p>{title}</p>
-          <p>{location}</p>
-          <p>by {author}</p>
-        </div>
-      </header>
-      <div className={classes.discription}>
-        <p className={classes.summary}>{summary}</p>
-      </div>
-    </article>
+      </article>
+    </Link>
   )
 }
