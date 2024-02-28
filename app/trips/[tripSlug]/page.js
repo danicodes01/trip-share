839eef5ff4b6ad246eb84b7d2d99cf0108c769b9
Daniel <danoladesign@gmail.com>
2024-02-28 16:23:36 -0500

    TS016: Set up basic html for trip detail page

--- app/trips/[tripSlug]/page.js
+++ app/trips/[tripSlug]/page.js
@@ -1,7 +1,21 @@
-export default function TripDetailPage() {
-    return (
+import image from 'next/image'
+export default function TripDetailPage () {
+  return (
+    <>
+    <header>
       <div>
-        <h1>Trip Detail Page</h1>
+        {/* <Image src={trip} alt='title' fill /> */}
       </div>
-    )
+      <div>
+        <h4>title</h4>
+        <p>by author</p>
+        <p>summary</p>
+      </div>
+    </header>
+    <main>
+      <p>description</p>
+    </main>
+    </>
+    
+  )
 }
