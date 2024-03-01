d18df7f8d6b68711f70fe6609a637d26ffdcad85
Daniel <danoladesign@gmail.com>
2024-03-01 15:19:49 -0500

    TS017: Called getTripBySlug from slug detail page to get specific trip / got slug from special params prop / rendered trip detail dynamically on page

--- app/trips/[tripSlug]/page.js
+++ app/trips/[tripSlug]/page.js
@@ -1,21 +1,24 @@
-import image from 'next/image'
-export default function TripDetailPage () {
+import Image from 'next/image'
+import { getTripBySlug } from '@/lib/trips'
+export default async function TripDetailPage ({ params }) {
+  const trip = await getTripBySlug(params.tripSlug)
+  const { title, author, summary, description } = trip
+
   return (
     <>
-    <header>
-      <div>
-        {/* <Image src={trip} alt='title' fill /> */}
-      </div>
-      <div>
-        <h4>title</h4>
-        <p>by author</p>
-        <p>summary</p>
-      </div>
-    </header>
-    <main>
-      <p>description</p>
-    </main>
+      <header>
+        <div>
+          <Image src={trip.image} alt='title' fill />
+        </div>
+        <div>
+          <h4>{title}</h4>
+          <p>by {author}</p>
+          <p>{summary}</p>
+        </div>
+      </header>
+      <main>
+        <p>{description}</p>
+      </main>
     </>
-    
   )
 }
