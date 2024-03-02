3b968835d944ba5f510200fc8822239c35f7537e
Daniel <danoladesign@gmail.com>
2024-03-02 16:40:49 -0500

    TS019: Updated code to in trip-item.js to handle url from images hosted on aws

--- components/trips/trip-item.js
+++ components/trips/trip-item.js
@@ -14,7 +14,7 @@ export default function TripItem ({
       <article className={classes.trip}>
         <header>
           <div className={classes.image}>
-            <Image src={image} alt={title} fill />
+            <Image src={`https://ourtrips.s3.amazonaws.com/photos/${image}`} alt={title} fill />
           </div>
           <div className={classes.info}>
             <p>{title}</p>
