8ccbdd0c8517757275f6a6034b3d7368a15f0e98
Daniel <danoladesign@gmail.com>
2024-03-04 19:29:34 -0500

    TS021: Build out basic html for trips/share page

--- app/trips/share/page.js
+++ app/trips/share/page.js
@@ -1,7 +1,35 @@
-export default function ShareTripPage() {
-    return (
-      <div>
+export default function ShareTripPage () {
+  return (
+    <>
+      <header>
         <p>Share Trip Page</p>
-      </div>
-    )
+      </header>
+      <main>
+        <form>
+          <div>
+            <p>
+              <label htmlFor='name'>name</label>
+              <input type='text' id='name' name='name' />
+            </p>
+            <p>
+              <label>email</label>
+              <input type='email' id='email' name='email' />
+            </p>
+          </div>
+          <p>
+            <label>title</label>
+            <input type='text' id='title' name='title' />
+          </p>
+          <p>
+            <label>summary</label>
+            <input type='text' id='summary' name='summary' />
+          </p>
+          <p>
+            <label>description</label>
+            <textarea id='description' name='description' rows={10} />
+          </p>
+        </form>
+      </main>
+    </>
+  )
 }
