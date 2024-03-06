79dca046c7c8c2a54546435154affb5762b819f5
Daniel <danoladesign@gmail.com>
2024-03-06 18:37:04 -0500

    TS022: Added imageInputRef and handleClick method to interact with image selector

--- components/trips/image-selector.js
+++ components/trips/image-selector.js
@@ -1,6 +1,15 @@
+'use client'
 
+import { useRef } from "react";
 import classes from "./image-selector.module.css";
 export default function ImageSelector({ label, name }) {
+    const imageInputRef = useRef()
+
+    function handleClick() {
+        if(imageInputRef.current) {
+            imageInputRef.current.click()
+        }
+      }
   
     return (
       <div className={classes.selector}>
@@ -12,10 +21,12 @@ export default function ImageSelector({ label, name }) {
             id={name}
             accept='image/png, image/jpeg'
             name={name}
+            ref={imageInputRef}
           />
           <button
             className={classes.btn}
             type='button'
+            onClick={handleClick}
           >
             Pick an Image
           </button>
