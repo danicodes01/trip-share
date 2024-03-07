b130099c738454c54a51b36bd0727c6849ad5ff6
Daniel <danoladesign@gmail.com>
2024-03-06 21:08:35 -0500

    TS023: Set up server actions

--- app/trips/share/page.js
+++ app/trips/share/page.js
@@ -1,5 +1,6 @@
 import ImageSelector from '@/components/trips/image-selector'
 import classes from './page.module.css'
+import {create} from '../../../lib/actions'
 export default function ShareTripPage () {
   return (
     <>
@@ -7,21 +8,17 @@ export default function ShareTripPage () {
         <p><span>share</span> your favorite memories</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={create}>
           <div className={classes.rows}>
             <p>
               <label htmlFor='name'>name</label>
               <input type='text' id='name' name='name' />
             </p>
-            <p>
-              <label>email</label>
-              <input type='email' id='email' name='email' />
-            </p>
-          </div>
           <p>
             <label>title</label>
             <input type='text' id='title' name='title' />
           </p>
+          </div>
           <p>
             <label>summary</label>
             <input type='text' id='summary' name='summary' />
