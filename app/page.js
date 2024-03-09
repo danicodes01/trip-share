e9e2ca8bf0a03a0cb9de55a95adc043ac4bd39cf
Daniel <danoladesign@gmail.com>
2024-03-09 02:21:15 -0500

    TSO26: Replaced carousel photos with Trips object photos / added server side call to getTrips in homePage and passed trips as props to carousel component

--- app/page.js
+++ app/page.js
@@ -1,18 +1,25 @@
 import classes from './page.module.css'
 import Carousel from '@/components/carousel/carousel'
 import Link from 'next/link'
-export default function Home () {
+import { getTrips } from '@/lib/trips'
+export default async function Home () {
+  const trips = await getTrips()
+
   return (
     <>
       <header className={classes.header}>
         <div className={classes.carousel}>
-          <Carousel />
+          <Carousel trips={trips} />
         </div>
       </header>
       <main className={classes.intro}>
-  <p><Link href='/trips/share' className={classes.link}>share</Link> and inspire others with your experiences</p>
-</main>
-
+        <p>
+          <Link href='/trips/share' className={classes.link}>
+            share
+          </Link>{' '}
+          and inspire others with your experiences
+        </p>
+      </main>
     </>
   )
 }
--- components/carousel/carousel.js
+++ components/carousel/carousel.js
@@ -1,50 +1,31 @@
 'use client';
 import { useEffect, useState } from "react";
 import Image from "next/image";
-
-import bugImg from "@/assets/bug.jpg";
-import cactusImg from "@/assets/cactus.jpg";
-import clocktowerImg from "@/assets/clocktower.jpg";
-import marsImg from "@/assets/mars.jpg";
-import romaImg from "@/assets/roma.jpg";
-import seadogImg from "@/assets/seadog.jpg"
-import wigImg from "@/assets/wig.jpg";
 import classes from "./carousel.module.css";
 
-
-const images = [
-  
-  { image: bugImg, alt: "A old bug" },
-  { image: cactusImg, alt: "A lovely cactus" },
-  { image: clocktowerImg, alt: "Latino Americano" },
-  { image: marsImg, alt: "A cat named Mars" },
-  { image: romaImg, alt: "Fountain in Roma" },
-  { image: seadogImg, alt: "A dog at the beach" },
-  { image: wigImg, alt: "A wig" },
-];
-
-export default function Carousel() {
+export default function Carousel({trips}) {
   const [photoIndex, setPhotoIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setPhotoIndex((prevIndex) =>
-        prevIndex < images.length - 1 ? prevIndex + 1 : 0
+        prevIndex < trips.length - 1 ? prevIndex + 1 : 0
       );
-    }, 5000);
+    }, 3000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [trips.length]);
 
   return (
     <div className={classes.carousel}>
-      {images.map((image, index) => (
+      {trips.map((trip, index) => (
 
         <Image
           key={index}
-          src={image.image}
+          src={`https://ourtrips.s3.amazonaws.com/photos/${trip.image}`}
           className={index === photoIndex ? classes.active : ''}
-          alt={image.alt}
+          alt={trip.alt}
+          layout="fill"
         />
       ))}
     </div>
