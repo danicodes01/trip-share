4be363309249a21cc8153d33ce593347b6706187
Daniel <danoladesign@gmail.com>
2024-03-09 19:40:33 -0500

    TS027: Added client side error handling and validation for 'trip/share' page

--- app/trips/share/page.js
+++ app/trips/share/page.js
@@ -13,22 +13,22 @@ export default function ShareTripPage () {
           <div className={classes.rows}>
             <p>
               <label htmlFor='name'>your name</label>
-              <input type='text' id='author' name='author' />
+              <input type='text' id='author' name='author' required/>
             </p>
             <p>
               <label htmlFor='name'>trip location</label>
-              <input type='text' id='location' name='location' />
+              <input type='text' id='location' name='location' required/>
             </p>
           </div>
           <p>
             <label>title</label>
-            <input type='text' id='title' name='title' />
+            <input type='text' id='title' name='title' required/>
           </p>
           <p>
             <label>description</label>
-            <textarea id='description' name='description' rows={10} />
+            <textarea id='description' name='description' rows={10} required/>
           </p>
-          <ImageSelector label='image' name='image' />
+          <ImageSelector label='image' name='image' required/>
           <p className={classes.submit}>
             <TripSubmit/>
           </p>
