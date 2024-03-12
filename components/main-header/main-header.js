a8eeba00a0d03327f1e12627cfbe950dae56afa8
Daniel <danoladesign@gmail.com>
2024-03-12 19:12:28 -0400

    TS030: Updated logo

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -7,7 +7,7 @@ export default function MainHeader() {
   return (
     <header className={classes.header}>
       <Link className={classes.logo} href='/'>
-        <Image src={logo} alt='logo' priority />
+        <p className={classes.highlight}>tripShare</p>
       </Link>
       <nav className={classes.nav}>
         <ul>
